d0a3157e43f4936420d31bddfdbc9faadbb0dc14
Lucas Fernandes Vieira <lucas-fv@outlook.com>
2018-03-26 21:37:14 -0300

    Support for parameters added
    Will install any parameter or any entry in a file, if a file is passed

--- robotconfigurator.py
+++ robotconfigurator.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import platform
 import subprocess
 
-
+# returns the running OS to decide where to search the installation scripts
 def get_os():
     if platform.system() == "Windows":
         return "Windows"
@@ -16,6 +17,7 @@ def get_os():
         else:
             return "Other"
 
+# append some text to a file, useful to set config strings to system files.
 def append_text_to_file(path, content):
     try:
         file = open(path, "a")
@@ -25,9 +27,11 @@ def append_text_to_file(path, content):
     except IOError:
         print("Could not create file", path)
 
+# returns the current linux kernel formatted like 414, 415, 49... used only to install virtualbox modules
 def get_linux_kernel():
     return subprocess.check_output(["uname", "-r"]).decode("utf-8").replace(".", "")[:3]
 
+# run the correct scripts to install program to the specified system
 def install(program, system):
     try:
         with open("Scripts/" + system + "/" + program, "r") as script:
@@ -42,20 +46,28 @@ def install(program, system):
     except IOError:
         print("Not supported")
 
-
+# starts the program
 if __name__ == "__main__":
-    if not get_os() == "other":
-        try:
-            with open("programs-to-install.txt", "r") as programs_to_install:
-                for line in programs_to_install:
-                    if (not line.startswith("#")) and (not line == "\n"):
-                        install(line.replace('\n', ''), get_os())
+    # check if the system is supported
+    if not get_os() == "Other":
+        for param in sys.argv[1:]:
+            # tries to install every parameter received
+            if os.path.isfile(param):
+                try:
+                    with open(param, "r") as programs_to_install:
+                        for line in programs_to_install:
+                            if (not line.startswith("#")) and (not line == "\n"):
+                                install(line.replace('\n', ''), get_os())
 
-        except KeyboardInterrupt:
-            print("Stopped by user")
+                except KeyboardInterrupt:
+                    print("Stopped by user")
 
-        except IOError:
-            print("Could not read programs-to-install.txt")
+                except IOError:
+                    print("Could not read the specified file")
+
+            # install every entry if a file is received (check programs-to-install.txt for an example)
+            else:
+                install(param, get_os())
 
     else:
         print("Unsupported OS :(")
